bef0fb7b1bf891c041ca7b7c9a69a679086d2eb6
alissatroiano <alissatroianonyc@gmail.com>
2021-12-27 22:08:03 -0500

    Updates

---
+++ precache-manifest.ca68b9294a66341defd92b0392db6ff3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f930f92c13ae50e03e65",
+    "url": "/Ravenous-App/static/css/main.ec693ddb.chunk.css"
+  },
+  {
+    "revision": "f930f92c13ae50e03e65",
+    "url": "/Ravenous-App/static/js/main.bfa72e8e.chunk.js"
+  },
+  {
+    "revision": "be06a80291020dee1541",
+    "url": "/Ravenous-App/static/js/runtime~main.0adf7a88.js"
+  },
+  {
+    "revision": "e9eea0971e8038d5e033",
+    "url": "/Ravenous-App/static/js/2.f6106045.chunk.js"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous-App/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous-App/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "26532af06461159e42df8fe86b4dade9",
+    "url": "/Ravenous-App/index.html"
+  }
+];

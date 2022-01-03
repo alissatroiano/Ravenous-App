305ff01d501ade4e49bc9b0d6b2b7a044afee520
alissatroiano <alissatroianonyc@gmail.com>
2022-01-03 10:47:08 -0500

    Create production build and import api key

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,31 +1,27 @@
-import config from './config';
-
-const apiKey = "'UimXxI7dai7nvLQvUMYEXTj1GSUYrDPpRsupjDFEDwF9O4pH196OQUop8e6SqdKYhWyBzbNjBff2sz0cmXu_msMVu4Ao9ET_4TTRNJkidw1wZGmOH6HRMvfSMOTDYXYx";
+const apiKey = 'UimXxI7dai7nvLQvUMYEXTj1GSUYrDPpRsupjDFEDwF9O4pH196OQUop8e6SqdKYhWyBzbNjBff2sz0cmXu_msMVu4Ao9ET_4TTRNJkidw1wZGmOH6HRMvfSMOTDYXYx';
 
 const Yelp = {
-  search(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+  async search(term, location, sortBy) {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
       headers: {
         Authorization: `Bearer ${apiKey}`
       }
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      if (jsonResponse.businesses) {
-        return jsonResponse.businesses.map(business => ({
-          id: business.id,
-          imageSrc: business.image_url,
-          name: business.name,
-          address: business.location.address1,
-          city: business.location.city,
-          state: business.location.state,
-          zipCode: business.location.zip_code,
-          category: business.categories[0].title,
-          rating: business.rating,
-          reviewCount: business.review_count
-        }));
-      }
     });
+    const jsonResponse = await response.json();
+    if (jsonResponse.businesses) {
+      return jsonResponse.businesses.map(business => ({
+        id: business.id,
+        imageSrc: business.image_url,
+        name: business.name,
+        address: business.location.address1,
+        city: business.location.city,
+        state: business.location.state,
+        zipCode: business.location.zip_code,
+        category: business.categories[0].title,
+        rating: business.rating,
+        reviewCount: business.review_count
+      }));
+    }
   }
 };
 
